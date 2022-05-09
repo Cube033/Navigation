906f909b599e8f85bc7cf59727305325a65426d9
Cube033 <cube033@me.com>
2022-05-09 10:07:11 +0700

    Исправление домашней работы по AutoLayout согласно рекомендациям

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -11,48 +11,56 @@ import UIKit
 class ProfileViewController: UIViewController {
     
     let profileHeaderView = ProfileHeaderView()
+    var changeTitleButton = UIButton()
    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setView()
+    }
+    
+    private func setView(){
         view.backgroundColor = .lightGray
         self.view.addSubview(profileHeaderView)
         self.title = "Profile"
-        
         profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
-        [
+        changeTitleButton = setChangeTitleButton()
+        setConstraints()
+        
+    }
+    
+    private func setChangeTitleButton() -> UIButton{
+        let changeTitleButton: UIButton = {
+            let button = UIButton()
+            button.backgroundColor = .blue
+            button.layer.cornerRadius = 4.0
+            button.setTitle("Изменить заголовок", for: .normal)
+            button.setTitleColor(.white, for: .normal)
+            button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        
+            button.layer.shadowColor = UIColor.black.cgColor
+            button.layer.shadowOffset = .init(width: 4, height: 4)
+            button.layer.shadowOpacity = 0.7
+            button.layer.shadowRadius = 4
+            button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            return button
+        }()
+        self.view.addSubview(changeTitleButton)
+        return changeTitleButton
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
             profileHeaderView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
             profileHeaderView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
             profileHeaderView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
-            profileHeaderView.heightAnchor.constraint(equalToConstant: 220)
-        ]
-        .forEach({$0.isActive = true})
-        
-        let changeTitleButton = UIButton()
-        setChangeTitleButton(button: changeTitleButton)
-    }
-    
-    private func setChangeTitleButton(button: UIButton){
-        button.backgroundColor = .blue
-        button.layer.cornerRadius = 4.0
-        button.setTitle("Изменить заголовок", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
-        
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.shadowOffset = .init(width: 4, height: 4)
-        button.layer.shadowOpacity = 0.7
-        button.layer.shadowRadius = 4
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-        
-        self.view.addSubview(button)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        [
-            button.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
-            button.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
-            button.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
-            button.heightAnchor.constraint(equalToConstant: 25)
-        ]
-        .forEach({$0.isActive = true})
+            profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
+            
+            changeTitleButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            changeTitleButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            changeTitleButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            changeTitleButton.heightAnchor.constraint(equalToConstant: 25)
+        ])
     }
     
     @objc private func buttonAction() {
